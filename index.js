acfcbf8bf9099391b9705ee0bca45645c67de166
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-08-17 13:44:45 +0100

    realtime messaging working

--- index.js
+++ index.js
@@ -2,10 +2,39 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
 
 server.listen(40001);
 console.log("server started");
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
+});
+
+io.sockets.on('connection',function(socket){
+    connections.push(socket);
+    console.log("Connected: $s sockets connected",connections.length);
+    
+    socket.on('disconnect',function(data){
+        users.splice(users.indexOf(socket.username),1);
+        connections.splice(connections.indexOf(socket),1);
+        console.log("Disconnected: $s sockets connected",connections.length);
+    });
+
+    socket.on('send message',function(data){
+        console.log(data);
+        io.sockets.emit('new message',{msg:data , username:socket.username});
+    });
+    socket.on('add user',function(data){
+        //callback(true);
+        socket.username = data;
+        console.log("new user"+data);
+        users.push(socket.username);
+        updateUsers();
+    });
+    function updateUsers(){
+        io.sockets.emit('new user',users);
+    }
 });
